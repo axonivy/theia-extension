d53280fa2cc79effa601324aa4421b8fca7b1cc2
Lukas Lieb <lukas.lieb@axonivy.com>
2020-08-25 09:04:25 +0200

    wip

--- tree-extension/src/browser/tree-iar-label-contribution.ts
+++ tree-extension/src/browser/tree-iar-label-contribution.ts
@@ -1,27 +1,20 @@
 import URI from "@theia/core/lib/common/uri";
-import { FileStatNode } from "@theia/filesystem/lib/browser/file-tree/file-tree";
+import { FileStatNode, DirNode } from "@theia/filesystem/lib/browser/file-tree/file-tree";
 import { FileTreeLabelProvider } from "@theia/filesystem/lib/browser/file-tree/file-tree-label-provider";
-import { injectable,  } from "inversify";
+import { injectable } from "inversify";
 
 @injectable()
 export class LabelExtensionLabelProviderContribution extends FileTreeLabelProvider {
-    
+
     canHandle(element: object): number {
-        if (FileStatNode.is(element)) {
-            let node = element.fileStat;
-            //let uri = new URI(element.fileStat.uri);
+        if (DirNode.is(element)) {
             let isIar = 0;
-            if (node.isDirectory)
-            {
-                node.children?.forEach(file => {
-                    if (new URI(file.uri).path.base === 'pom.xml') {
-                        isIar = super.canHandle(element)+1;
-                    }
-                });
-            }
-            /*if (uri.path.ext === '.my') {
-                return super.canHandle(element)+1;
-            }*/
+            let node = element.fileStat;
+            node.children?.forEach(file => {
+                if (new URI(file.uri).path.base === 'pom.xml') {
+                    isIar = super.canHandle(element)+1;
+                }
+            });
             return isIar;
         }
         return 0;
